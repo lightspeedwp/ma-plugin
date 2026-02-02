fa33c440458b8e4ab822ccdc3c2c01da96d25260
Warwick <krugazul@gmail.com>
2026-02-02 21:37:23 +0200

    Add clock icon SVGs and improve debugging in helper functions
    - Added outline and solid versions of the clock icon SVGs to the source-icons directory.
    - Updated the render function for the digital magazine field display block to include debugging information for icon type and name.
    - Enhanced the icon SVG retrieval function with debugging logs to verify file existence and paths.

--- build/blocks/digital_magazine-field-display/index.asset.php
+++ build/blocks/digital_magazine-field-display/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '3fddb8af2de911996e64');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '03f25a056d4cef517f82');
