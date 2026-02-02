c92475e3200ad688152fc57b57331ebeabb8635b
Warwick <krugazul@gmail.com>
2026-02-02 22:04:07 +0200

    Updating the icon layout.

--- build/blocks/digital_magazine-field-display/index.asset.php
+++ build/blocks/digital_magazine-field-display/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '31c7d83dfe64fea6662b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '129b30017f207d044b2b');
--- build/blocks/digital_magazine-field-display/render.php
+++ build/blocks/digital_magazine-field-display/render.php
@@ -84,7 +84,7 @@ if ( ! function_exists( 'ma_plugin_render_digital_magazine_field_display' ) ) {
 
 		// Start building the output.
 		$output = sprintf(
-			'<div class="%s" style="flex-wrap: nowrap; vertical-align: top;">',
+			'<div class="%s" style="flex-wrap: nowrap; gap: var(--wp--preset--spacing--20);">',
 			esc_attr( implode( ' ', $wrapper_classes ) )
 		);
 
@@ -93,9 +93,7 @@ if ( ! function_exists( 'ma_plugin_render_digital_magazine_field_display' ) ) {
 			$svg_content = ma_plugin_get_icon_svg( $icon_type, $icon_name );
 			if ( ! empty( $svg_content ) ) {
 				$output .= sprintf(
-					'<div class="wp-block-group is-layout-flex" style="flex-wrap: nowrap; vertical-align: middle;">
-						<span class="block-icon-svg" style="font-size: inherit; display: inline-block;">%s</span>
-					</div>',
+				'<div class="wp-block-group is-layout-flex" style="flex-wrap: nowrap;">%s</div>',
 					$svg_content // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- SVG content is sanitized in ma_plugin_get_icon_svg().
 				);
 			}
