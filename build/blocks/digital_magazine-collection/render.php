df6ba86aaf6ad406b41a20cc3d10237070c72a7a
Warwick <krugazul@gmail.com>
2026-02-02 21:30:22 +0200

    Refactor render function names in block JSON and PHP files for consistency; add new field groups for digital magazines, SFWD courses, and webinars

--- build/blocks/digital_magazine-collection/render.php
+++ build/blocks/digital_magazine-collection/render.php
@@ -9,8 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! function_exists( 'ma_plugin_render_digital-magazine_collection' ) ) {
-	function ma_plugin_render_digital-magazine_collection( $attributes, $content, $block ) {
+if ( ! function_exists( 'ma_plugin_render_digital_magazine_collection' ) ) {
+	function ma_plugin_render_digital_magazine_collection( $attributes, $content, $block ) {
 		// Output markup for the CPT1 collection block.
 		return '<div class="wp-block-ma_plugin-digital-magazine-collection">' .
 			'<p>' . esc_html__( 'CPT1 collection block output.', 'ma-plugin' ) . '</p>' .
--- build/blocks/digital_magazine-slider/render.php
+++ build/blocks/digital_magazine-slider/render.php
@@ -9,8 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! function_exists( 'ma_plugin_render_digital-magazine_slider' ) ) {
-	function ma_plugin_render_digital-magazine_slider( $attributes, $content, $block ) {
+if ( ! function_exists( 'ma_plugin_render_digital_magazine_slider' ) ) {
+	function ma_plugin_render_digital_magazine_slider( $attributes, $content, $block ) {
 		// Output markup for the slider block.
 		return '<div class="wp-block-ma_plugin-digital-magazine-slider">' .
 			'<p>' . esc_html__( 'Slider block output.', 'ma-plugin' ) . '</p>' .
